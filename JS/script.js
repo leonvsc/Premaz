d16fa580314c48628177786c2e6cb059f65458fd
Niels Warnaar <niels-warnaar@live.nl>
2021-10-09 23:21:21 +0200

    Removed the css from the cycle images.

--- JS/script.js
+++ JS/script.js
@@ -5,16 +5,17 @@ function start() {
     const cycleImages = document.getElementById("cycleImages");
     const imageDir = "img/Cycle images/";
     const delayInSeconds = 1;
-    const images = ["img-1.jpg", "img-2.jpg"];
+    const images = ["img-1.jpg", "img-2.jpg", "img-3.jpg"];
     let num = 0;
     const changeImage = function () {
         const len = images.length;
         cycleImages.src = imageDir + images[num++];
-        if (num == len) {
+        if (num === len) {
             num = 0;
         }
     };
-    setInterval(changeImage, delayInSeconds * 2500);
+    setInterval(changeImage, delayInSeconds * 3500);
 }
 window.onload = function () {
     start();
+};
