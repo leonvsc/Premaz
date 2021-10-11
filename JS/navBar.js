204f33e7b11499c11ed11253a2d90d5a4720dd64
Niels Warnaar <niels-warnaar@live.nl>
2021-10-11 22:07:18 +0200

    Tweaked the mobile navbar

---
+++ JS/navBar.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav_links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav_link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}
