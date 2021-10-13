2ef4eed71d5832530eaec266bfdf34d9e1a78c3b
Leon <leonvsc@users.noreply.github.com>
2021-10-13 16:08:22 +0200

    Added the header with the image

--- js/script.js
+++ js/script.js
@@ -1,34 +1,20 @@
 let opacity = 0;
-let intervalID = 0;
 
 function start() {
     const cycleImages = document.getElementById("cycleImages");
     const imageDir = "img/Cycle images/";
     const delayInSeconds = 1;
-    const images = [
-        "achtergrond-afbeelding-1.1.jpg",
-        "afbeelding-alinea-1.jpg",
-    ];
+    const images = ["img-1.jpg", "img-2.jpg", "img-3.jpg"];
     let num = 0;
     const changeImage = function () {
         const len = images.length;
         cycleImages.src = imageDir + images[num++];
-        if (num == len) {
+        if (num === len) {
             num = 0;
         }
     };
-    setInterval(changeImage, delayInSeconds * 2500);
+    setInterval(changeImage, delayInSeconds * 10000);
 }
 window.onload = function () {
     start();
 };
-/* function hide() {
-    opacity = Number(window.getComputedStyle(cycleImages).getPropertyValue("opacity"));
-        if (opacity > 0) {
-            opacity = opacity - 0.1;
-        }
-        else {
-            clearInterval(intervalID);
-            cycleImages.style.opacity = opacity;
-        }
-} */
