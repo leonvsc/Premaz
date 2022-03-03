f986f4db5a6ee783d716b2165a98694ed96ec606
Leon <leonvsc@users.noreply.github.com>
2022-03-03 13:18:39 +0100

    Added NOT NULL to some attributes

--- sql/premaz.sql
+++ sql/premaz.sql
@@ -3,86 +3,86 @@ CREATE DATABASE IF NOT EXISTS `premaz`;
 USE `premaz`;
 
 CREATE TABLE `Customers` (
-	`CustomerNumber` INT,
-    `FirstName` VARCHAR(35),
-    `LastName` VARCHAR(35),
-    `Address` VARCHAR(35),
-    `HouseNumber` VARCHAR(6),
-    `PostalCode` VARCHAR(6),
-    `City` VARCHAR(35),
+	`CustomerNumber` INT NOT NULL,
+    `FirstName` VARCHAR(35) NOT NULL,
+    `LastName` VARCHAR(35) NOT NULL,
+    `Address` VARCHAR(35) NOT NULL,
+    `HouseNumber` VARCHAR(6) NOT NULL,
+    `PostalCode` VARCHAR(6) NOT NULL,
+    `City` VARCHAR(35) NOT NULL,
     CONSTRAINT `CustomersPK`
     	PRIMARY KEY (`CustomerNumber`)
 );
 
 CREATE TABLE `Accounts` (
-    `Email` VARCHAR(30),
+    `Email` VARCHAR(30) NOT NULL,
     `CT_CustomerNumber` INT,
     `SC_ShoppingCartID` INT,
     `OD_OrderNumber` VARCHAR(8),
-    `Password` VARCHAR(128),
-    `Role` VARCHAR(20),
+    `Password` VARCHAR(128) NOT NULL,
+    `Role` VARCHAR(20) NOT NULL,
     CONSTRAINT `AccountsPK`
     	PRIMARY KEY (`Email`)
 );
 
 CREATE TABLE `Orders` (
-    `OrderNumber` VARCHAR(8),
+    `OrderNumber` VARCHAR(8) NOT NULL,
     `PM_PaymentID` VARCHAR(8),
     `SC_ShoppingcartID` INT,
-    `DeliveryAddress` VARCHAR(35),
-    `DeliveryHouseNumber` VARCHAR(6),
-    `DeliveryPostalCode` VARCHAR(6),
-    `DeliveryCity` VARCHAR(35),
+    `DeliveryAddress` VARCHAR(35) NOT NULL,
+    `DeliveryHouseNumber` VARCHAR(6) NOT NULL,
+    `DeliveryPostalCode` VARCHAR(6) NOT NULL,
+    `DeliveryCity` VARCHAR(35) NOT NULL,
     `Track&Trace` VARCHAR(15),
-    `OrderStatus` VARCHAR(20),
+    `OrderStatus` VARCHAR(20) NOT NULL,
     CONSTRAINT `OrdersPK`
     	PRIMARY KEY (`OrderNumber`)
 );
 
 CREATE TABLE `Payments` (
-    `PaymentID` VARCHAR(8),
+    `PaymentID` VARCHAR(8) NOT NULL,
     `IV_InvoiceNumber` VARCHAR(8),
-    `Price` FLOAT,
-    `Currency` VARCHAR(3),
-    `Method` VARCHAR(20),
-    `PaymentStatus` VARCHAR(20),
+    `Price` FLOAT NOT NULL,
+    `Currency` VARCHAR(3) NOT NULL,
+    `Method` VARCHAR(20) NOT NULL,
+    `PaymentStatus` VARCHAR(20) NOT NULL,
     CONSTRAINT `PaymentsPK`
     	PRIMARY KEY (`PaymentID`)
 );
 
 CREATE TABLE `Invoices` (
-    `InvoiceNumber` VARCHAR(8),
+    `InvoiceNumber` VARCHAR(8) NOT NULL,
     `VATNumber` VARCHAR(20),
-    `InvoiceAddress` VARCHAR(35),
-    `InvoiceHouseNumber` VARCHAR(6),
-    `InvoicePostalCode` VARCHAR(6),
-    `InvoiceCity` VARCHAR(35),
+    `InvoiceAddress` VARCHAR(35) NOT NULL,
+    `InvoiceHouseNumber` VARCHAR(6) NOT NULL,
+    `InvoicePostalCode` VARCHAR(6) NOT NULL,
+    `InvoiceCity` VARCHAR(35) NOT NULL,
     CONSTRAINT `InvoicesPK`
     	PRIMARY KEY (`InvoiceNumber`)
 );
 
 CREATE TABLE `Products` (
-    `SKU` VARCHAR(30),
-    `Price` FLOAT,
-    `Stock` INT,
-    `Category` VARCHAR(35),
-    `ShortDescription` TEXT,
+    `SKU` VARCHAR(30) NOT NULL,
+    `Price` FLOAT NOT NULL,
+    `Stock` INT NOT NULL,
+    `Category` VARCHAR(35) NOT NULL,
+    `ShortDescription` TEXT NOT NULL,
     `Specifications` TEXT,
     CONSTRAINT `ProductsPK`
     	PRIMARY KEY (`SKU`)
 );
 
 CREATE TABLE `ShoppingCartRows` (
-    `ShoppingCartRowID` INT,
-    `PD_SKU` VARCHAR(30),
-    `SC_ShoppingCartID` INT,
-    `Quantity` INT,
+    `ShoppingCartRowID` INT NOT NULL,
+    `PD_SKU` VARCHAR(30) NOT NULL,
+    `SC_ShoppingCartID` INT NOT NULL,
+    `Quantity` INT NOT NULL,
     CONSTRAINT `ShoppingCartRowsPK`
     	PRIMARY KEY (`ShoppingCartRowID`)
 );
 
 CREATE TABLE `ShoppingCarts` (
-    `ShoppingCartID` INT,
+    `ShoppingCartID` INT NOT NULL,
     `SCR_ShoppingCartRowID` INT,
     CONSTRAINT `ShoppingCartsPK`
     	PRIMARY KEY (`ShoppingCartID`)
