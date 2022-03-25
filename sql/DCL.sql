b27f032dec12618c1ef5fb359f10af424885473d
Leon <leonvsc@users.noreply.github.com>
2022-03-25 12:47:04 +0100

    Added localhost to the queries, made a revoke query and made a query to check the permissions

--- sql/DCL.sql
+++ sql/DCL.sql
@@ -1,23 +1,29 @@
 -- Systeembeheerder
-CREATE USER 'Systeembeheerder'@'premaz' IDENTIFIED WITH caching_sha2_password BY 'Password1!';
-GRANT ALL PRIVILEGES ON *.* TO 'Systeembeheerder'@'premaz' WITH GRANT OPTION;
+CREATE USER 'systeembeheerder'@'localhost' IDENTIFIED WITH caching_sha2_password BY 't#X9!daZHxFnzuxHr#tyzhwVP';
+GRANT ALL PRIVILEGES ON premaz.* TO 'systeembeheerder'@'localhost' WITH GRANT OPTION;
 
 -- Medewerker
-CREATE USER 'Medewerker'@'premaz' IDENTIFIED WITH caching_sha2_password BY 'Password1!';
-GRANT SELECT, UPDATE, INSERT ON premaz.Products TO 'Medewerker'@'premaz' WITH GRANT OPTION;
-GRANT SELECT, UPDATE ON premaz.Orders TO 'Medewerker'@'premaz' WITH GRANT OPTION;
+CREATE USER 'medewerker'@'localhost' IDENTIFIED WITH caching_sha2_password BY 't#X9!daZHxFnzuxHr#tyzhwVP';
+GRANT SELECT, UPDATE, INSERT ON premaz.Products TO 'medewerker'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, UPDATE ON premaz.Orders TO 'medewerker'@'localhost' WITH GRANT OPTION;
 
 -- Administratie
-CREATE USER 'Administratie'@'premaz' IDENTIFIED WITH caching_sha2_password BY 'Password1!';
-GRANT SELECT, UPDATE ON premaz.Invoices TO 'Administratie'@'premaz' WITH GRANT OPTION;
-GRANT SELECT, UPDATE ON premaz.Payments TO 'Administratie'@'premaz' WITH GRANT OPTION;
-GRANT SELECT, UPDATE, INSERT ON premaz.Customers TO 'Administratie'@'premaz' WITH GRANT OPTION;
-GRANT SELECT, UPDATE, INSERT ON premaz.BillingAddress TO 'Administratie'@'premaz' WITH GRANT OPTION;
-GRANT SELECT, UPDATE, INSERT ON premaz.ShippingAddress TO 'Administratie'@'premaz' WITH GRANT OPTION;
+CREATE USER 'administratie'@'localhost' IDENTIFIED WITH caching_sha2_password BY 't#X9!daZHxFnzuxHr#tyzhwVP';
+GRANT SELECT, UPDATE ON premaz.Invoices TO 'administratie'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, UPDATE ON premaz.Payments TO 'administratie'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, UPDATE, INSERT ON premaz.Customers TO 'administratie'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, UPDATE, INSERT ON premaz.BillingAddress TO 'administratie'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, UPDATE, INSERT ON premaz.ShippingAddress TO 'administratie'@'localhost' WITH GRANT OPTION;
 
 -- Klant
-CREATE USER 'Klant'@'premaz' IDENTIFIED WITH caching_sha2_password BY 'Password1!';
-GRANT SELECT, UPDATE, INSERT ON premaz.Accounts TO 'Klant'@'premaz' WITH GRANT OPTION;
-GRANT SELECT ON premaz.Invoices TO 'Klant'@'premaz' WITH GRANT OPTION;
-GRANT SELECT ON premaz.Orders TO 'Klant'@'premaz' WITH GRANT OPTION;
-GRANT SELECT, UPDATE, INSERT ON premaz.ShoppingCarts TO 'Klant'@'premaz' WITH GRANT OPTION;
+CREATE USER 'klant'@'localhost' IDENTIFIED WITH caching_sha2_password BY 't#X9!daZHxFnzuxHr#tyzhwVP';
+GRANT SELECT, UPDATE, INSERT ON premaz.Accounts TO 'klant'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON premaz.Invoices TO 'klant'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON premaz.Orders TO 'klant'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, UPDATE, INSERT ON premaz.ShoppingCarts TO 'klant'@'localhost' WITH GRANT OPTION;
+
+-- Revoke permissies
+REVOKE SELECT ON premaz.Invoices FROM 'klant'@'localhost';
+
+-- Check de permissies
+SHOW GRANTS FOR 'klant'@'localhost';
