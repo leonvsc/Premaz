5b2fe7ebcd9c728c1ca761fcdd85b1b5cce099c3
Leon <leonvsc@users.noreply.github.com>
2022-04-29 17:11:52 +0200

    Created database class and database connection

---
+++ php/data/database.php
@@ -0,0 +1,30 @@
+<?php
+
+class database
+{
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+
+    public function connect()
+    {
+        $this->servername = "sqladmin.premaz.nl";
+        $this->username = "premaz";
+        $this->password = "FnN2K*HZu8@96Ey9iC!e@Dn52nz@WF";
+        $this->dbname = "premaz";
+
+        try {
+            $conn = new PDO("mysql:host=$this->servername;$this->dbname", $this->username, $this->password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
+
+$db = new database();
+
+$db->connect();
