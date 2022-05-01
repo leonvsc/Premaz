74476c5cfc124d4c10c9db3c3ac1176d80120770
Leon <leonvsc@users.noreply.github.com>
2022-05-01 16:45:28 +0200

    Made a simple forgot password page

---
+++ php/view/forgotPassword.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot password</title>
+</head>
+
+<body>
+    <h1 style="text-align: center">Forgot Password - Premaz Webshop</h1>
+    <form action="" method="post">
+        <table class="">
+            <tr>
+                <p style="text-align: center">Type in your email-address and fill in a new password</p>
+            </tr>
+            <tr>
+                <th>Email</th>
+                <td><input type="text" name="email" class="" required></td>
+            </tr>
+
+            <tr>
+                <th>New password</th>
+                <td><input type="password" name="password" class="" required></td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="submit" value="Reset Password" class="">
+                    <a href="login.php" class="">Go back to Login</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
