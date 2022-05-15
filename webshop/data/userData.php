3bb8e822a85f5f30505f03ba995c5a9965400f45
Leon <leonvsc@users.noreply.github.com>
2022-05-15 14:34:57 +0200

    Created signUpInsert function

--- webshop/data/userData.php
+++ webshop/data/userData.php
@@ -21,7 +21,19 @@ class userData
         }
     }
 
-    public function signUpInsert()
+    public function signUpInsert($accountModel, $customerModel)
     {
+        $email = $accountModel->getAccount;
+        $password = $accountModel->getPassword;
+        $customerNumber = $customerModel->getCustomerNumber;
+        $firstName = $customerModel->getFirstName;
+        $lastName = $customerModel->getLastName;
+        $phoneNumber = $customerModel->getPhoneNumber;
+
+        // SQL
+        $accountInsert = "INSERT INTO `Accounts` (`Email`, `Password`, `Role`) VALUES (''{$email}'', '{$password}', 'User');";
+        $customerInsert = "INSERT INTO `Customers` (`CustomerNumber`, `AC_Email`, `BA_BillingAddressID`, `SA_ShippingAddressID`, `SC_ShoppingCartID`, `FirstName`, `LastName`, `PhoneNumber`) VALUES ('{$customerNumber}', '{$email}', NULL, NULL, NULL, '{$firstName}', '{$lastName}', {$phoneNumber});";
+
+        $result = mysqli_query($this->db->connect(), $accountInsert);
     }
 }
