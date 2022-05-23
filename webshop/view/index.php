9ff670eec3d540abc22ec6c1fe1f13a82130b117
Leon <leonvsc@users.noreply.github.com>
2022-05-23 18:12:24 +0200

    Made a session for login

--- webshop/view/index.php
+++ webshop/view/index.php
@@ -1,1 +1,19 @@
-<h1> hi </h1>
+<?php
+session_start();
+
+if (isset($_SESSION["email"])) {
+?>
+    <li><a href="#"><?php echo $_SESSION["email"]; ?></a></li>
+    <li><a href="../includes/logout.inc.php">LOGOUT</a></li>
+<?php
+} else {
+?>
+    <li><a href="signup.php">SIGN UP</a></li>
+    <li><a href="login.php">Login</a></li>
+<?php
+}
+
+?>
+
+
+<h1> hi <?php echo $_SESSION["email"] ?> </h1>
