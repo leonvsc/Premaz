a3310a6874ea84a3b597a5452c881a8f1157209c
Leon <leonvsc@users.noreply.github.com>
2022-05-24 17:24:31 +0200

    Protecting the adminpanel page so normal users cant go to it and you have to login to access the page

--- webshop/view/adminpanel.php
+++ webshop/view/adminpanel.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION["email"])) {
+    header("Location: ../view/login.php");
+} elseif ($_SESSION["role"] != "Admin") {
+    header("Location: ../view/index.php"); // TODO: Redirect to own account page or something else?
+}
+?>
 
 <h1>Welkom op het admin panel</h1>
 
