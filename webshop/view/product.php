17a138a9b6dee434da2814e170aab1f87ca6f126
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2022-06-05 14:28:13 +0200

    Made a page which output product data regarding the specified SKU

--- webshop/view/product.php
+++ webshop/view/product.php
@@ -13,9 +13,16 @@
     <?php
     require_once "../data/productData.php";
     $data = new productData;
-    echo '<h3> Dit is een test </h3>';
-    echo var_dump($data->getById('KEY-MEM-LOW-001'));
+    $productInfo = ($data->getById('KEY-MEM-LOW-001'));
+    $price = $productInfo[0]->getPrice();
+    $SKU = $productInfo[0]->getSKU();
+    $category = $productInfo[0]->getCategory();
+    $stock = $productInfo[0]->getStock();
     ?>
+    <h5>Het product kost : <?php echo $price ?></h5>
+    <h5>De SKU is: <?php echo $SKU ?></h5>
+    <h5>De catagorie is: <?php echo $category ?></h5>
+    <h5>De voorraad is: <?php echo $stock ?></h5>
 </body>
 
 </html>
