3061fe62298738fa25b4eb9b385de78d23175a12
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2022-06-05 15:46:47 +0200

    Made the productoverview page and links to the specific products

--- webshop/view/product.php
+++ webshop/view/product.php
@@ -12,8 +12,27 @@
     <h1 style="text-align: center">Products - Premaz Webshop</h1>
     <?php
     require_once "../data/productData.php";
+    
+   
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
+        $url = "https://";   
+        else  
+        $url = "http://";   
+    // Append the host(domain name, ip) to the URL.   
+    $url.= $_SERVER['HTTP_HOST'];   
+
+    // Append the requested resource location to the URL   
+    $url.= $_SERVER['REQUEST_URI'];     
+    // Use parse_url() function to parse the URL 
+    // and return an associative array which
+    // contains its various components
+    $url_components = parse_url($url);
+    
+    // Use parse_str() function to parse the
+    // string passed via URL
+    parse_str($url_components['query'], $params);
     $data = new productData;
-    $productInfo = ($data->getById('KEY-MEM-LOW-001'));
+    $productInfo = $data->getById($params['SKU']);
     $price = $productInfo[0]->getPrice();
     $SKU = $productInfo[0]->getSKU();
     $category = $productInfo[0]->getCategory();
---
+++ webshop/view/productOverview.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1 style="text-align: center">Products - Premaz Webshop</h1>
+    <?php
+    require_once "../data/productData.php";
+    $data = new productData;
+    $allProducts = $data->getAll();
+
+    for ($i = 0; $i < count($allProducts); ++$i)
+    {
+        echo '<br />',
+        '<a href="product.php/?SKU=',$allProducts[$i]->getSKU(),'">',
+        'Prijs van het product: €', 
+        $allProducts[$i]->getPrice(),
+        '<br />',
+        'SKU van het product: ', 
+        $allProducts[$i]->getSKU(),
+        '<br />',
+        'Category van het product: ', 
+        $allProducts[$i]->getCategory(),
+        '<br />', 
+        'Voorraad van het product: ',
+         $allProducts[$i]->getStock(),
+         '</a>', 
+        '<br />',
+        '<hr>';
+    }
+    ?>
+
+</body>
+</html>
