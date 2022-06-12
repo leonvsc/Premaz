4bdfda9a5fb41ad0f106390ee14d6f941c9521c5
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2022-06-12 22:00:33 +0200

    Made a small change to the image upload

--- webshop/view/imageUpload.php
+++ webshop/view/imageUpload.php
@@ -33,7 +33,7 @@ if (!isset($_SESSION["email"])) {
       <input type="submit" value="Upload Image" name="submit">
     </form>
     <?php
-    $target_dir = "img/products";
+    $target_dir = "img/products/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
